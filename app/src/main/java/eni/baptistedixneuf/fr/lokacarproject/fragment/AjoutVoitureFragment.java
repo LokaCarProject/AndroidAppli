94cb0153083ac9651aac4cf8ccad578831071c25
Unknown <meltoz@outlook.fr>
2017-05-03 16:40:31 +0200

    Ajout fonction ajout voiture

--- app/src/main/java/eni/baptistedixneuf/fr/lokacarproject/fragment/AjoutVoitureFragment.java
+++ app/src/main/java/eni/baptistedixneuf/fr/lokacarproject/fragment/AjoutVoitureFragment.java
@@ -4,12 +4,21 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import eni.baptistedixneuf.fr.lokacarproject.MenuActivity;
 import eni.baptistedixneuf.fr.lokacarproject.R;
+import eni.baptistedixneuf.fr.lokacarproject.bo.Categorie;
 import eni.baptistedixneuf.fr.lokacarproject.bo.Voiture;
+import eni.baptistedixneuf.fr.lokacarproject.dao.VoitureDao;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,7 +28,7 @@ import eni.baptistedixneuf.fr.lokacarproject.bo.Voiture;
  * Use the {@link AjoutVoitureFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AjoutVoitureFragment extends Fragment {
+public class AjoutVoitureFragment extends Fragment  implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,7 +37,8 @@ public class AjoutVoitureFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    
+
+    private Voiture  voiture;
 
     private OnFragmentInteractionListener mListener;
 
@@ -52,6 +62,8 @@ public class AjoutVoitureFragment extends Fragment {
 
     public AjoutVoitureFragment() {
         // Required empty public constructor
+
+
     }
 
     @Override
@@ -66,8 +78,55 @@ public class AjoutVoitureFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_ajout_voiture, container, false);
+        voiture = new Voiture();
+        Button b = (Button)view.findViewById(R.id.saveVoiture);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText marque = (EditText)view.findViewById(R.id.editMarque);
+                EditText modele = (EditText)view.findViewById(R.id.editModel);
+                EditText immatriculation = (EditText)view.findViewById(R.id.editImat);
+                EditText prix = (EditText)view.findViewById(R.id.editPrix);
+                RadioButton cat1 = (RadioButton)view.findViewById(R.id.cat1);
+                EditText couleur = (EditText)view.findViewById(R.id.editCouleur);
+
+                if(verification(view))
+                {
+                    voiture.setMarque(marque.getText().toString());
+                    voiture.setModele(modele.getText().toString());
+                    voiture.setCouleur(couleur.getText().toString());
+                    voiture.setImmatriculation(immatriculation.getText().toString());
+                    voiture.setPrix(Double.parseDouble(prix.getText().toString()));
+
+                    Categorie cat = new Categorie();
+                    String categorie;
+                    if(cat1.isChecked())
+                    {
+                        categorie = "categorie1";
+                    }
+                    else
+                    {
+                        categorie = "categorie2";
+                    }
+                    voiture.setCategorie(cat);
+                    VoitureDao voitureDao = new VoitureDao(getActivity());
+                    voitureDao.add(voiture);
+                    Log.d("test", "Voiture enregistrer");
+                }
+                else
+                {
+                    Toast.makeText(getContext(), "Tous les champs doivent etre renseigné", Toast.LENGTH_LONG);
+                }
+
+            }
+        });
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajout_voiture, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -96,6 +155,66 @@ public class AjoutVoitureFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+    public void onClick(View v)
+    {
+        EditText marque = (EditText)v.findViewById(R.id.editMarque);
+        EditText modele = (EditText)v.findViewById(R.id.editModel);
+        EditText immatriculation = (EditText)v.findViewById(R.id.editImat);
+        EditText prix = (EditText)v.findViewById(R.id.editPrix);
+        RadioButton cat1 = (RadioButton)v.findViewById(R.id.cat1);
+        EditText couleur = (EditText)v.findViewById(R.id.editCouleur);
+
+        if(verification(v))
+        {
+            voiture.setMarque(marque.getText().toString());
+            voiture.setModele(modele.getText().toString());
+            voiture.setCouleur(couleur.getText().toString());
+            voiture.setImmatriculation(immatriculation.getText().toString());
+            voiture.setPrix(Double.parseDouble(prix.getText().toString()));
+
+            Categorie cat = new Categorie();
+            String categorie;
+            if(cat1.isChecked())
+            {
+                categorie = "categorie1";
+            }
+            else
+            {
+                categorie = "categorie2";
+            }
+            voiture.setCategorie(cat);
+            VoitureDao voitureDao = new VoitureDao(getActivity());
+            voitureDao.add(voiture);
+            Log.d("test", "Voiture enregistrer");
+        }
+        else
+        {
+            Toast.makeText(getContext(), "Tous les champs doivent etre renseigné", Toast.LENGTH_LONG);
+        }
+    }
+
+    public boolean verification(View v)
+    {
+        boolean ok = true;
+
+        EditText marque = (EditText)v.findViewById(R.id.editMarque);
+        EditText modele = (EditText)v.findViewById(R.id.editModel);
+        EditText immatriculation = (EditText)v.findViewById(R.id.editImat);
+        EditText prix = (EditText)v.findViewById(R.id.editPrix);
+        EditText couleur = (EditText)v.findViewById(R.id.editCouleur);
+        if(marque ==null  || modele==null || immatriculation == null || prix == null || couleur == null)
+        {
+            ok=false;
+        }
+        else if(couleur.getText().toString().isEmpty() || marque.getText().toString().isEmpty() || modele.getText().toString().isEmpty() || immatriculation.getText().toString().isEmpty() || prix.getText().toString().isEmpty())
+        {
+            ok = false;
+        }
+
+        return ok;
+
     }
 
 }
